cba9b709ef6d138eb5ef636157268cdb594bc402
root <root@kimsufi-ylb>
2016-05-10 11:06:32 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>Hervé La Luciole<h1/>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Hervé La Luciole</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+
+	<h1>Hervé La Luciole<h1/>
+
+</body>
+</html>
